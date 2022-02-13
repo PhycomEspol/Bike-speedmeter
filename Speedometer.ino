16d469c00b63aeb2a190cfcb230225e7801d3c4b
Alina Carpio <alinacarpio@pop-os.localdomain>
2022-02-12 21:03:35 -0500

    update velocidad

--- Speedometer.ino
+++ Speedometer.ino
@@ -1,39 +1,49 @@
 // include the library code:
 #include <LiquidCrystal.h>
 
+#define RADIO_LLANTA 0.37
+#define PI           3.14
+
+#define BUTTON       8
+
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-int button = 8;
-int val = 0;
-int count = 0;
-int vueltas;
-float radioLlanta = 0.37; //Radio de la llanta en cm
 
+double distancia{0};
 
 void setup() {
-  
   lcd.begin(16, 2);
-
-  pinMode(button, INPUT);  
-  
+  pinMode(BUTTON, INPUT); 
 }
 
+void printVelocidad(int x, int y) {
+    int segundos{millis() / 1000};
+    int velocidad{distancia / (segundos < 1 ? 1 : segundos)};
+  	lcd.setCursor(x, y);
+  	lcd.print("Speed: ");
+    lcd.print(max(velocidad, 0));
+  	lcd.print("m/s");
+}
+
+void printDistancia(int x, int y) {
+  	lcd.setCursor(0, 1);
+  	lcd.print("Dist:  ");
+  	lcd.print(distancia);
+  	lcd.print("m");
+}
+
+void avanzar() {
+  distancia += 2 * PI * RADIO_LLANTA; 
+}
 
 void loop() { 
+  int val {digitalRead(BUTTON)};
   
-  val = digitalRead(8);
-  if (val == HIGH ) {
-    vueltas = count ++;
-	delay(250);
+  if (val == HIGH) {
+	avanzar();
   }
-  	lcd.setCursor(0, 0);
-    lcd.print("Velocidad: ");
-    lcd.setCursor(0, 1);
-  	float distancia = (2 * 3.14 * radioLlanta);
-      
-    lcd.print("Recorrido:");
-	lcd.print(distancia*vueltas);
-  	lcd.print("m");
-
+  
+  printVelocidad(0, 0);
+  printDistancia(0, 1);
 }
  
